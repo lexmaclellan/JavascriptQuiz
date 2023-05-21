6cf61c1009f11dd20014b3707c331f2ceb215c78
lexrayne <lexrayne@outlook.com>
2023-05-20 23:08:53 -0300

    Added button styling and first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var timer = 60;
+var timer = 75;
 
 var body = document.body;
 var timeEl = document.querySelector(".time");
@@ -6,6 +6,11 @@ var quizEl = document.querySelector(".quiz");
 var beginButton = document.querySelector("#begin");
 var h2El = document.createElement("h2");
 var pEl = document.createElement("p");
+
+var buttonEl1 = document.createElement("button");
+var buttonEl2 = document.createElement("button");
+var buttonEl3 = document.createElement("button");
+var buttonEl4 = document.createElement("button");
 
 beginButton.addEventListener("click", function() {
     setTime();
@@ -27,10 +32,16 @@ function setTime() {
 
 function beginQuiz() {
     quizEl.textContent = "";
-    h2El.textContent="Question 1"
-    pEl.textContent = "Hello World";
+    h2El.textContent = "Commonly used datatypes DO NOT include:";
+    buttonEl1.textContent = "1. strings";
+    buttonEl2.textContent = "2. booleans";
+    buttonEl3.textContent = "3. alerts";
+    buttonEl4.textContent = "4. numbers";
     quizEl.appendChild(h2El);
-    quizEl.appendChild(pEl);
+    quizEl.appendChild(buttonEl1);
+    quizEl.appendChild(buttonEl2);
+    quizEl.appendChild(buttonEl3);
+    quizEl.appendChild(buttonEl4);
 };
 
 function endQuiz() {
