cb0085374217edb9233b431f24e1da45e8e12252
lexrayne <lexrayne@outlook.com>
2023-05-20 21:40:23 -0300

    Added placeholder for first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,32 @@
 var timer = 60;
 
+var body = document.body;
 var timeEl = document.querySelector(".time");
+var quizEl = document.querySelector(".quiz");
 var beginButton = document.querySelector("#begin");
+var h2El = document.createElement("h2");
+var pEl = document.createElement("p");
 
 beginButton.addEventListener("click", function() {
     setTime();
+    beginQuiz();
 });
 
 function setTime() {
     var timerInterval = setInterval(function() {
         timer--;
         timeEl.textContent = "Time Remaining: " + timer;
-        console.log(timer);
+        
         if (timer === 0) {
             clearInterval(timerInterval);
         }
     }, 1000);
-}
+};
+
+function beginQuiz() {
+    quizEl.textContent = "";
+    h2El.textContent="Question 1"
+    pEl.textContent = "Hello World";
+    quizEl.appendChild(h2El);
+    quizEl.appendChild(pEl);
+};
